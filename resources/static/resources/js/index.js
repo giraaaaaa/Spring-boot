8f1a3eb8bcaa3ebae6adb72bbc65b4aa13e180bc
landamaizer <landamaizer@gmail.com>
2019-06-12 14:04:01 +0900

    14:03

--- resources/static/resources/js/index.js
+++ resources/static/resources/js/index.js
@@ -1,18 +1,68 @@
-// alert('자바스크립트 연결 성공!');
+var app = (function(){
+    let init = function(){
+       login_form();
 
-var wrapper = document.querySelector('#wrapper');
-wrapper.innerHTML ='<h1>SPA 시작</h1>'
-+'<div id="target">삭제할 내용</div>'
-+'<button id="btn">지워버리자</button>'
-+'<button id="btn2">추가하자</button>';
 
-var btn = document.querySelector('#btn');
-var target = document.querySelector('#target');
-btn.addEventListener('click',function(){
-    target.innerHTML = '';
-})
-var btn2 = document.querySelector('#btn2');
-btn2.addEventListener('click',function(){
-    var temp = document.createTextNode('새로 추가됨');
-    target.appendChild(temp);
-})
+    }
+    let login_form = function(){
+        var wrapper = document.querySelector('#wrapper');
+        wrapper.innerHTML =
+        '<form action="/action_page.php">'
+        +'  First name:<br>'
+        +'  <input type="text" name="firstname" value="Mickey">'
+        +'  <br>'
+        +'  Last name:<br>'
+        +'  <input type="text" name="lastname" value="Mouse">'
+        +'  <br><br>'
+        // +'  <input id="login" type="submit" value="로그인">'
+        // +'  <input id="signin" type="submit" value="회원가입">'
+        +'</form> '
+        +'<button id="login">로그인</button>'
+        +'<button id="sign">회원가입</button>'
+
+         var sign = document.querySelector('#sign');
+         var login = document.querySelector('#login');
+         sign.addEventListener('click',function(){
+           wrapper.innerHTML =
+            +'<form action="/action_page.php">'
+            +'ID:<br>'
+            +'<input type="text" name="firstname" value="Mickey">'
+            +'<br>'
+            +'IW:<br>'
+            +'<input type="text" name="lastname" value="Mouse">'
+            +'<br>'
+            +'email:<br>'
+            +'<input type="text" name="lastname" value="Mouse">'
+            +'<br><br>'
+            +'</form> '
+            +'<button id="okay">확인</button>'
+            var okay = document.querySelector('#okay');
+            okay.addEventListener('click',function(){
+                wrapper.innerHTML =
+                '<form action="/action_page.php">'
+                +'  First name:<br>'
+                +'  <input type="text" name="firstname" value="Mickey">'
+                +'  <br>'
+                +'  Last name:<br>'
+                +'  <input type="text" name="lastname" value="Mouse">'
+                +'  <br><br>'
+                +'</form> '
+                +'<button id="login">로그인</button>'
+                +'<button id="sign">회원가입</button>'
+                 });
+    
+         });
+         
+        //  var btn2 = document.querySelector('#btn2');
+        //  btn2.addEventListener('click',function(){
+        //  var temp = document.createTextNode('새로 추가됨');
+        //  target.appendChild(temp);
+        //  })
+    }
+
+
+
+
+
+    return {init : init} ; 
+})()

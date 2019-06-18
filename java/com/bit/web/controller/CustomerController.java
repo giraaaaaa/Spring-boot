76a8d4c5a15c9fff2618c473c33fc6cbb6fcf247
landamaizer <landamaizer@gmail.com>
2019-06-18 15:39:38 +0900

    gg

--- java/com/bit/web/controller/CustomerController.java
+++ java/com/bit/web/controller/CustomerController.java
@@ -4,36 +4,46 @@ import com.bit.web.domain.CustomerDTO;
 import com.bit.web.service.CustomerService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * CoustomerController
- */
+* CustomerController
+*/
 @RestController
+@RequestMapping("/customers")
 public class CustomerController {
-    @Autowired CustomerService customerService;
-    @Autowired CustomerDTO customer;
-    
-    @RequestMapping("/count")
-    public String index() {
-        System.out.println("CustomerController count() 경로로 들어옴");
-        int count = customerService.countAll();
-        System.out.println("고객의 총인원 : " + count);
-        return count+"";
-    }
+   @Autowired CustomerService customerService;
+   @Autowired CustomerDTO customer;
 
-    @RequestMapping("/login/{customerId}/{password}")
-    public String login(@PathVariable("customerId")String id,
-                        @PathVariable("password")String pass) {
-        System.out.println("ajax로 넘어온 ID : " + id);
-        System.out.println("ajax로 넘어온 PW : " + pass);
-        customer.setCustomerId(id);
-        customer.setPassword(pass);
-        System.out.println("DB에서 넘어온 이름 : " + customerService.login(customer).getCustomerName());
-        
-        return (customerService.login(customer) != null) 
-                ? "SUCCESS" : "Fail";
-    }
+
+   @RequestMapping("/count")  //루트 URL
+   public String count() {
+       System.out.println("CustomerController count() 경로로 들어옴");
+       int count = customerService.countAll();
+       System.out.println("고객의 총인원 : " + count);
+       return "총 고객수 : "+count+"";  //views에 customer.html이아닌 제이슨으로 바뀜, 주소를 넘기는게 아닌 데이터를 넘김     대신에 받는쪽에서 ajax사용
+   }
+
+   @GetMapping("/{customerId}/{password}")
+   public CustomerDTO login(@PathVariable("customerId")String id,
+                       @PathVariable("password")String pass){
+       customer.setCustomerId(id);
+       customer.setPassword(pass);
+       return customerService.login(customer);
+   }
+
+   @PostMapping("")
+   public CustomerDTO join(@RequestBody CustomerDTO param){
+       System.out.println("=====post mapping======");
+       System.out.println(param.getCustomerId());
+       System.out.println(param.getPassword());
+       System.out.println(param.getCustomerName());
+       return customerService.login(customer);
+   }
 }
--- resources/static/resources/js/index.js
+++ resources/static/resources/js/index.js
@@ -1,102 +1,107 @@
-var app = (()=>{
-    let init = ()=>{ //맨 처음 보여줄 화면
-       login_form();
+// use
+var app =  {
+   $wrapper : $wrapper = document.querySelector('#wrapper'),
+   init : ()=>{
+       $wrapper.innerHTML = app.login_form();
+       let join_btn = document.querySelector('#join-btn');
+       join_btn.addEventListener('click',()=>{
+           $wrapper.innerHTML = app.join_form();
+           document.getElementById('confirm-btn')
+           .addEventListener('click', e=>{
+               e.preventDefault();
+               alert('조인버튼 클릭');
+               app.join();
+           });
+       });
+       let login_btn = document.querySelector('#login-btn');
+       login_btn.addEventListener('click',e=>{
+           e.preventDefault();
+           alert('로그인 버튼 클릭');
+           id = document.getElementById('customerId').value;
+           pass = document.getElementById('password').value;
+           let xhr = new XMLHttpRequest();
+           xhr.open('GET', 'customers/'+id+'/'+pass, true);
+           xhr.onload=()=>{
+               if(xhr.readyState=== 4 && xhr.status === 200){
+                   if(xhr.responseText){
+                       $wrapper.innerHTML = app.mypage();
+                   }else{
+                       $wrapper.innerHTML = app.login_form();
+                   }
+               }
+           };
+           xhr.send();
+
+       });
 
 
-    }
-    let login_form = ()=>{ //시작화면
-        let wrapper = document.querySelector('#wrapper');
-        wrapper.innerHTML =
-        '<form action="/action_page.php">'
-        +'  First name:<br>'
-        +'  <input type="text" id="customerId" name="customerId">'
-        +'  <br>'
-        +'  Last name:<br>'
-        +'  <input type="text" id="password" name="password">'
-        +'  <br><br>'
-        +'  <input id="login_btn" type="button" value="로그인">'
-        +'  <input id="signin_btn" type="button" value="회원가입">'
-        +'</form> '
-        // 회원가입 버튼의 아이디를 만들어준다. id는 필요할때만 만들도록 해야한다.
-            let signin_btn = document.querySelector('#signin_btn')
-            signin_btn.addEventListener('click', ()=>{
-                    join_form();
-                })//클릭시 join form으로 이동
-       
-            let login_btn = document.querySelector('#login_btn');
-            login_btn.addEventListener('click', ()=>{
-                alert("로그인 버튼 클릭");
-                id = document.getElementById('customerId').value;
-                pass = document.getElementById('password').value;
-                let xhr = new XMLHttpRequest(),
-                    method = 'GET',
-                    url = 'login/'+ id +'/' + pass;
-                xhr.open(method,url,true);
-                xhr.onreadystatechange=()=>{
-                    if(xhr.readyState === 4 && xhr.status===200 ){
-                        let d = xhr.responseText;
-                        if(d ==='SUCCESS'){
-                            let wrapper = document.querySelector('#wrapper');
-                            wrapper.innerHTML = '<h1>마이페이지!!!</h1>'
-                            alert('ajax 성공'+xhr.responseText);
-                        }else{
-                            let wrapper = document.querySelector('#wrapper');
-                            wrapper.innerHTML =
-                            '<form action="/action_page.php">'
-                            +'  First name:<br>'
-                            +'  <input type="text" id="customerId" name="customerId">'
-                            +'  <br>'
-                            +'  Last name:<br>'
-                            +'  <input type="text" id="password" name="password">'
-                            +'  <br><br>'
-                            +'  <input id="login_btn" type="button" value="로그인">'
-                            +'  <input id="signin_btn" type="button" value="회원가입">'
-                            +'</form> '
-                        }
-                    }
-                }
-                xhr.send();
-            });
-    }   
-    let count =()=>{
-        let xhr = new XMLHttpRequest();
-        method = 'GET';
-        url='count';
-        xhr.open(method, url, true);
 
-        xhr.onreadystatechange=()=>{
-            if(xhr.readyState===4 && xhr.status == 200){
-                alert("성공");
-                let wrapper = document.querySelector('#wrapper')
-                wrapper.innerHTML = '<h1 id="count">'+xhr.responseText+'</h1>'   
+   },
+
+   join : ()=>{
+       let xhr = new XMLHttpRequest()
+       let data = {
+        customerId : document.getElementById("customerId").value,
+        customerName : document.getElementById("customerName").value,
+        password : document.getElementById("password").value
+       };
+       xhr.open('POST','customers',true);
+       xhr.setRequestHeader('Content-type','application/json;charset=UTF-8');
+       xhr.onload=()=>{
+            if(xhr.readyState === 4 && xhr.status === 200){
+                let d = xhr.responseText;
+                alert('회원가입 성공' + d.result);
+            }else{
+                alert('회원가입 실패');
             }
-        }
-        xhr.send();
-    }
-    let join_form = ()=>{
-        let wrapper = document.querySelector('#wrapper');
-                  wrapper.innerHTML =
-           '<form action="/action_page.php">'
-           +'  I  D:<br>'
-           +'  <input type="text" name="firstname" value="">'
-           +'  <br>'
-           +'  P  W:<br>'
-           +'  <input type="text" name="lastname" value="">'
-           +'  <br>'
-           +'  email:<br>'
-           +'  <input type="text" name="lastname" value="">'
-           +'  <br><br>'
-           +'  </form> '
-           +'  <button id="okay">확인</button>'
-        let okay = document.querySelector('#okay');
-        okay.addEventListener('click',()=>{
-            login_form();
-        })//로그인 폼으로 이동
-        
-    }
+       }
+    //    
+       xhr.send(JSON.stringify(data));
+   },
+
+
+   count : ()=>{
+       var xhr = new XMLHttpRequest();
+       
+       xhr.open('GET', 'customers/count', true);
+       xhr.onload=()=>{
+           if(xhr.readyState===4 && xhr.status === 200){
+               alert('성공');
+               let wrapper = document.querySelector('#wrapper');
+               wrapper.innerHTML = '총 고객수 : <h1>'+xhr.responseText+'</h1>'
+           }
+       }
+       xhr.send();
+   }
+};
 
 
 
+app.mypage =()=>{
+   return '<h1>마이페이지</h1> ';
+};
 
-    return {init : init} ; 
-})()
+app.login_form=()=>{
+   return '<form action="/action_page.php">'
+   +'  First name:<br>'
+   +'  <input type="text" id="customerId" name="customerId">'
+   +'  <br>'
+   +'  Last name:<br>'
+   +'  <input type="text" id="password" name="password">'
+   +'  <br><br>'
+   +'  <input id="login-btn" type="button" value="LOGIN">'
+   +'  <input id="join-btn" type="button" value="JOIN">'
+   +'</form> ';
+};
+app.join_form=()=>{
+   return '<form> I D<br>'
+   +'    <input type="text" id="customerId" name="customerId"><br>'
+   +'    P W<br>'
+   +'    <input type="password" id="password" name="password"><br>'
+   +'    Name<br>'
+   +'    <input type="text"  id="customerName" name="customerName"><br>'
+   +'    <br><br>'
+   +'    <input id="confirm-btn" type="submit" value="확인">'
+   +'    <input id="cancel-btn" type="reset" value="취소">'
+   +'</form>';
+}
